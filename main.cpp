9326da4c25776644524428265dffc261e8b56fc3
z <240164005@qq.com>
2023-07-15 08:55:40 +0800

    需要优化链接顺序

--- main.cpp
+++ main.cpp
@@ -1,29 +1,61 @@
 #include <QCoreApplication>
-#include <QDebug>
+#include <QThread>
+#include <grpcpp/grpcpp.h>
+#include "example.grpc.pb.h"
 
+using example::Greeter;
+using example::HelloRequest;
+using example::HelloResponse;
 
-int main(int argc, char *argv[])
-{
+class GreeterImpl final : public Greeter::Service {
+    ::grpc::Status SayHello(::grpc::ServerContext* context, const HelloRequest* request, HelloResponse* response) override {
+        std::string name = request->name();
+        std::string message = "Hello, " + name + "!";
+        response->set_message(message);
+        return ::grpc::Status::OK;
+    }
+};
+
+void RunServer() {
+    std::string server_address("0.0.0.0:50051");
+    GreeterImpl service;
+
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(server_address, ::grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    std::unique_ptr<::grpc::Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+    server->Wait();
+}
+
+int main(int argc, char** argv) {
     QCoreApplication app(argc, argv);
 
-//    // 创建gRPC通道和存根
-//    std::shared_ptr<grpc::Channel> channel = grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
-//    std::unique_ptr<example::Greeter::Stub> stub = example::Greeter::NewStub(channel);
+//    // Start the gRPC server in a separate thread
+//    QThread* server_thread = new QThread();
+//    QObject::connect(server_thread, &QThread::started, RunServer);
+//    server_thread->start();
 
-//    // 创建请求
-//    example::HelloRequest request;
+//    // Create a gRPC channel to connect to the server
+//    std::shared_ptr<::grpc::Channel> channel = ::grpc::CreateChannel("localhost:50051", ::grpc::InsecureChannelCredentials());
+
+//    // Create a Greeter client
+//    std::unique_ptr<Greeter::Stub> stub = Greeter::NewStub(channel);
+
+//    // Prepare the request
+//    HelloRequest request;
 //    request.set_name("Alice");
 
-//    // 发起gRPC调用
-//    grpc::ClientContext context;
-//    example::HelloResponse response;
-//    grpc::Status status = stub->SayHello(&context, request, &response);
+//    // Call the SayHello RPC
+//    HelloResponse response;
+//    ::grpc::ClientContext context;
+//    ::grpc::Status status = stub->SayHello(&context, request, &response);
 
-//    // 处理响应
 //    if (status.ok()) {
-//        qDebug() << "Received response: " << QString::fromStdString(response.message());
+//        std::cout << "Server replied: " << response.message() << std::endl;
 //    } else {
-//        qDebug() << "RPC failed with error code: " << status.error_code() << ", " << QString::fromStdString(status.error_message());
+//        std::cout << "RPC failed with error code: " << status.error_code() << ", error message: " << status.error_message() << std::endl;
 //    }
 
     return app.exec();
